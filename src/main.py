5990c6f51685007b1819882dd851312624bc331f
0xCosydell <david.awatere@gmail.com>
2024-06-20 12:42:27 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,12 +8,12 @@ except ImportError:
 
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets["openai_key"]
-st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.title("Chat with the CicadaVPN knowledge, powered by ChatGPT 💬🦙")
+st.info("Check out the full tutorial to use the CicadaVPN service in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {"role": "assistant", "content": "Ask me a question about the CicadaVPN service!"}
     ]
 
 @st.cache_resource(show_spinner=False)
